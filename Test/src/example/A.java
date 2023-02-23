48dcba251c2902a07470a2e92700819949c0da4c
Amol Bendale <bendaleamol533@gmail.com>
2023-02-23 15:06:16 +0530

    Second commit

--- Test/src/example/A.java
+++ Test/src/example/A.java
@@ -4,5 +4,12 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java");
+		A a=new A();
+		a.m1();
+	}
+	
+	public void m1() 
+	{
+		System.out.println("method m1");
 	}
 }
