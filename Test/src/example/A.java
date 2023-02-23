a03942ef8fe9cf6278c3be9877a8eff0d46c662d
Amol-Bendale96 <106249400+Amol-Bendale96@users.noreply.github.com>
2023-02-23 15:09:42 +0530

    Update A.java
    Added m2 method in class A

--- Test/src/example/A.java
+++ Test/src/example/A.java
@@ -6,10 +6,16 @@ public class A {
 		System.out.println("Hello java");
 		A a=new A();
 		a.m1();
+		a.m2();
 	}
 	
 	public void m1() 
 	{
 		System.out.println("method m1");
+	}
+	public void m2()
+	{
+	
+	System.out.println("Method m2");
 	}
 }
